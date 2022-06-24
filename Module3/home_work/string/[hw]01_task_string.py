1bd0e0fe34801e7e19d899d6ad0c7dd9b530ce81
Geeez22 <69629344+Geeez22@users.noreply.github.com>
2022-06-24 18:12:51 +0300

    Update [hw]01_task_string.py

--- Module3/home_work/string/[hw]01_task_string.py
+++ Module3/home_work/string/[hw]01_task_string.py
@@ -1,4 +1,9 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+text = 'fdf....,,,2321'
+text_len = len(text)
+points = text.count('.')
+comma = text.count(',')
+if points == comma:
+    print('same')
