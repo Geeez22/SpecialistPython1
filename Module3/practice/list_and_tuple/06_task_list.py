22520de812740918dd3e3533e57f5a363e96ea37
Geeez22 <69629344+Geeez22@users.noreply.github.com>
2022-06-27 20:02:34 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,16 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+numbers_list = []
+new_number_list = []
+if first_number >= second_number:
+    for n in range(second_number, first_number):
+        numbers_list.append(n)
+elif second_number >= first_number:
+    for n in range(first_number, second_number):
+        numbers_list.append(n)
+for numbers in numbers_list:
+    if numbers % 3 == 0:
+        new_number_list.append(numbers)
+print(new_number_list)
+
