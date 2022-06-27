b1d0a6acaee2224b0db23e610652caab083179d9
Geeez22 <69629344+Geeez22@users.noreply.github.com>
2022-06-27 20:46:34 +0300

    Update 01_my_abs.py

--- Module4/practice/funcs_part0/01_my_abs.py
+++ Module4/practice/funcs_part0/01_my_abs.py
@@ -1,8 +1,12 @@
-def my_abs(...):
-    ...
-    return ...
+def my_abs(a):
+
+    if a == 0:
+        c = a
+    else:c = a*a/a
+    return int(c)
 
 
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+
